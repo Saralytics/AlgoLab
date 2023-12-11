bb2887d9c069c906ab2e4270768c68959f483ee4
Saralytics <61179765+Saralytics@users.noreply.github.com>
2023-12-11 16:45:51 +0400

    Update palindrome_number.py

--- MathProblems/palindrome_number.py
+++ MathProblems/palindrome_number.py
@@ -1,1 +1,18 @@
+# palindrome number is a number that is the same read from right to left or left to right
+# example: 12321, 78987, 6
+# write a function to check if an input number n is a palindrome number
+# hint: construct a new number by traverse through the last digit
+
+def palindrome(n):
+  reverse = 0
+  last_digit = 0
+  tmp = n
+  while tmp > 0:
+    last_digit = tmp % 10
+    print(f"last digit is{last_digit}" )
+    reverse = reverse * 10 + last_digit
+    print(f"reverse is {reverse}")
+    tmp = tmp//10
+    print(f"{tmp}")
+  print(n == reverse)
 
